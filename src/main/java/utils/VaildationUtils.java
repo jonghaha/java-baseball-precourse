af478e4cd2b1b4e52dd171c8372412a621a4d2b1
Jongha Baek <bgh9197@naver.com>
2021-10-06 01:09:38 +0900

    feat: 유저가 입력한 숫자 검증
    1-9숫자인지, 세자리숫자인지, 중복이 있는지 검증

--- src/main/java/utils/VaildationUtils.java
+++ src/main/java/utils/VaildationUtils.java
@@ -1,7 +1,55 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 public class VaildationUtils {
-	public static boolean vaildNumber(int num) {
-		return num >= 1 && num <= 9 ? true : false;
+	private final int INPUT_MAX_LENGTH = 3;
+	private final String REGEX = "[0-9]+";
+
+	/*
+	* 세자리 숫자 검증
+	* */
+	public void isThreeDigit(String userValue) {
+		if (userValue.length() != INPUT_MAX_LENGTH) {
+			throw new IllegalArgumentException("[ERROR] 세자리 숫자가 아닙니다.");
+		}
+	}
+
+	/*
+	* 숫자 및 1~9 숫자 검증
+	* */
+	public void isNumber(String userValue) {
+		if (!userValue.matches(REGEX)) {
+			throw new IllegalArgumentException("[ERROR] 문자는 입력할 수 없고 1 ~ 9 사이의 숫자만 입력해주세요.");
+		}
+	}
+
+	/*
+	* 중복 검증
+	* */
+	public List<Integer> isOverlap(String userValue) {
+		Set<Integer> userNumber = separateNumber(userValue);
+		if (userNumber.size() != INPUT_MAX_LENGTH) {
+			throw new IllegalArgumentException("[ERROR] 중복되는 수가 있습니다.");
+		}
+
+		return new ArrayList<>(userNumber);
+	}
+
+	/*
+	 * 숫자로 분리하여 Set으로 변환
+	 * */
+	private Set<Integer> separateNumber(String userValue) {
+		int inputValue = Integer.parseInt(userValue);
+		Set<Integer> inputNumber = new LinkedHashSet<>();
+
+		inputNumber.add(inputValue / 100); // 100의 자리
+		inputNumber.add((inputValue % 100) / 10); // 10의 자리
+		inputNumber.add(inputValue % 10); // 1의 자리
+
+		return inputNumber;
 	}
 }
--- src/test/java/utils/VaildationUtilsTest.java
+++ src/test/java/utils/VaildationUtilsTest.java
@@ -1,15 +1,36 @@
 package utils;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class VaildationUtilsTest {
+	VaildationUtils vaildationUtils = new VaildationUtils();
+
 	@Test
-	void 숫자가_1_9_사이인가() {
-		assertThat(VaildationUtils.vaildNumber(0)).isFalse();
-		assertThat(VaildationUtils.vaildNumber(10)).isFalse();
-		assertThat(VaildationUtils.vaildNumber(1)).isTrue();
-		assertThat(VaildationUtils.vaildNumber(9)).isTrue();
+	void 세자리_숫자인가() {
+		String userInput = "12345";
+
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+			vaildationUtils.isThreeDigit(userInput);
+		}).withMessageContaining("[ERROR] 세자리 숫자가 아닙니다.");
+	}
+
+	@Test
+	void 숫자이면서_1_9_사이인가() {
+		String userInput = "가나다";
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			vaildationUtils.isNumber(userInput);
+		}).withMessageContaining("[ERROR] 문자는 입력할 수 없고 1 ~ 9 사이의 숫자만 입력해주세요.");
+	}
+
+	@Test
+	void 중복이있는가() {
+		String userInput = "111";
+
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			vaildationUtils.isOverlap(userInput);
+		}).withMessageContaining("[ERROR] 중복되는 수가 있습니다.");
 	}
 }
